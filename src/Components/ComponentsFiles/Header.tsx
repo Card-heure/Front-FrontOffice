41bcb79db4356b7498c36c5a221824505e47378a
SilvusTV <hugo.combe.84@gmail.com>
2024-02-29 15:20:03 +0100

    add roboto Font and finish header

--- src/Components/ComponentsFiles/Header.tsx
+++ src/Components/ComponentsFiles/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className={"header-bg h-32 flex items-center"}>
+    <div className={"header-bg h-32 flex items-center border-b-black border-b-4"}>
       <h1 className={"text-6xl font-bold mx-16"}>C'h</h1>
     </div>
   )

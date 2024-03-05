9d7dabffaa9b816932e68637e8e0cb7637d8dbd6
madison owens <mowens@myges.fr>
2024-03-05 18:18:13 +0100

    Created the title Card for the Landing Page

---
+++ src/Components/ComponentsFiles/TitleCard.tsx
@@ -0,0 +1,24 @@
+export default function TitleCard() {
+    return (
+        <>
+            <div className = "introCard">
+                <div className = "title">
+                    <h2> Card'heure </h2>
+                    <p className = "one"> /Cardheure/ </p>
+                    <p className = "two"> nom masculin </p>
+                </div>
+
+                <div className = "definition">
+                    <ol>
+                        <li className = "one"> La plateforme pour les étudiants, par des étudiants. </li>
+                        <li className = "two"> Un regroupement moderne de méthodes d'apprentissage
+                             comprenant des cartes flash, des QCM et des fichiers
+                             texte qui permet aux étudiants d'étudier à la fois de
+                             manière indépendante et collaborative.
+                        </li>
+                    </ol>
+                </div>
+            </div>
+        </>
+    );
+}
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,10 +1,12 @@
 import Header from "#ComponentsFiles/Header.tsx";
+import TitleCard from "#ComponentsFiles/TitleCard.tsx";
 
 export default function Main() {
   return (
     <>
       <Header/>
       <div>Home</div>
+        <TitleCard/>
     </>
   )
 }

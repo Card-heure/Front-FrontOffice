c31699e33a0fe532f3592093903ac135d241734c
madison owens <mowens@myges.fr>
2024-04-06 10:23:18 +0200

    first half of making the sort menu functional

--- src/Components/ComponentsFiles/HomePage/SubjectSort.tsx
+++ src/Components/ComponentsFiles/HomePage/SubjectSort.tsx
@@ -1,13 +1,105 @@
+import React, { useState } from "react";
+
+export default function SubjectSort() {
+    const [sortOptionsHidden, setSortOptionsHidden] = useState(true);
+    const [sortType, setSortType] = useState("Newest Subjects First");
+    const [subjects, setSubjects] = useState([
+        "Economics",
+        "Algebra",
+        "English",
+        "Statistics",
+        "AP Government",
+    ]);
+
+    const toggleSortOptions = () => {
+        setSortOptionsHidden(!sortOptionsHidden);
+    };
+
+    const sortSubjects = (type) => {
+        let sortedSubjects = [...subjects]; // Create a copy of the subjects array
+        switch (type) {
+            case "Newest Subjects First":
+                sortedSubjects.sort((a, b) => b.localeCompare(a));
+                break;
+            case "Oldest Subjects First":
+                sortedSubjects.sort((a, b) => a.localeCompare(b));
+                break;
+            case "A - Z":
+                sortedSubjects.sort((a, b) => a.localeCompare(b));
+                break;
+            case "Z - A":
+                sortedSubjects.sort((a, b) => b.localeCompare(a));
+                break;
+            default:
+                break;
+        }
+        setSubjects(sortedSubjects); // Update the subjects state with the sorted array
+        setSortType(type); // Update the sort type
+        setSortOptionsHidden(true); // Hide sort options after selection
+    };
+
+    return (
+        <div className="subjectSection border-t-[black] border-t-[1px] border-t-[solid] mt-[65px]">
+            <div className="sort w-[30%] mx-[auto] mt-[30px] text-center flex text-[rgb(87,_88,_87)]">
+                <div className="sortBy h-[30px] w-[14%] ml-[19%]">
+                    <p className="overflow-hidden h-[30px] leading-[30px]">
+                        {" "}
+                        Sort By:
+                    </p>
+                </div>
+
+                <div className="sortMenu w-[48%] mr-[19%] relative bg-[rgb(255,_255,_255)]">
+                    <div className="menuSelected">
+                        <button
+                            className="sortSelected overflow-hidden h-[30px] leading-[30px] text-[black]"
+                            onClick={toggleSortOptions}
+                            style={{ borderBottom: '1px solid black' }}
+                        >
+                            {sortType}
+                        </button>
+                    </div>
+
+                    <ul className={sortOptionsHidden ? "sortOptions hidden" : "sortOptions"} style={{ position: 'absolute', top: '100%', left: 0, width: '100%', border: '1px solid grey', borderRadius: '12px', background: 'white', padding: '2px 4px 16px'}}>
+                        <li onClick={() => sortSubjects("Newest Subjects First")}>
+                            <button>Newest Subjects First</button>
+                        </li>
+                        <li onClick={() => sortSubjects("Oldest Subjects First")}>
+                            <button>Oldest Subjects First</button>
+                        </li>
+                        <li onClick={() => sortSubjects("A - Z")}>
+                            <button>A - Z</button>
+                        </li>
+                        <li onClick={() => sortSubjects("Z - A")}>
+                            <button>Z - A</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <ul className="subject flex mx-[auto] w-[80%] flex-wrap mt-[65px]">
+                {subjects.map((subject, index) => (
+                    <li key={index}>{subject}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+
+/* import {useState} from "react";
+
 export default function SubjectSort() {
 
-    const sortOptionsHidden = "sortOptions w-[100%] mx-[auto] absolute hidden";
+    const SortOptionsHidden = "sortOptions w-[100%] mx-[auto] absolute hidden";
     const sortOptionsShow = "sortOptions w-[100%] pb-[16px] bg-[rgb(255,_255,_255)] border-[black] border-[solid] border-[1px] rounded-[16px] absolute";
+    const [subjects, setSubjects] = useState(["Economics", "Algebra", "English", "Statistics", "AP Government"]);
+
     const showMenu = () => {
-        if (sortOptions.className == sortOptionsHidden) {
+        if (sortOptions.className == SortOptionsHidden) {
             sortOptions.className = sortOptionsShow;
         }
         else if (sortOptions.className == sortOptionsShow) {
-            sortOptions.className = sortOptionsHidden;
+            sortOptions.className = SortOptionsHidden;
         }
     }
 
@@ -16,7 +108,6 @@ export default function SubjectSort() {
         newestFirst.className = "";
         oldestFirst.className = "hidden";
         atoZ.className = "";
-        ztoA.className = "";
     }
 
     const sortAtoZ = () => {
@@ -25,6 +116,8 @@ export default function SubjectSort() {
         oldestFirst.className = "";
         atoZ.className = "hidden";
         ztoA.className = "";
+        const sorted = subjects.sort((a,b) => a.localeCompare(b));
+        setSubjects(sorted);
     }
 
     const sortZtoA = () => {
@@ -62,7 +155,7 @@ export default function SubjectSort() {
                                     onClick = {showMenu}>Newest Subjects First</button>
                         </div>
 
-                        <ul className = {sortOptionsHidden} id = "sortOptions">
+                        <ul className = {SortOptionsHidden} id = "sortOptions">
                             <li className = "hidden" id = "newestFirst" onClick = {sortNewest}>
                                 <button>Newest Subjects First</button>
                             </li>
@@ -81,13 +174,13 @@ export default function SubjectSort() {
                 </div>
 
                 <ul className = "subject flex mx-[auto] w-[80%] flex-wrap mt-[65px]">
-                    <li>Economics</li>
-                    <li>Algebra</li>
-                    <li>English</li>
-                    <li>Statistics</li>
-                    <li>AP Government</li>
+                    {subjects.map((subject, index) =>
+                        (
+                        <li key={index}>{subject}</li>
+                        ))}
                 </ul>
             </div>
         </>
     )
 }
+*/
